6d9c1802ad1c915eeed51a3e018586f30b2bd254
David Stainton <dstainton415@gmail.com>
2024-04-25 15:48:40 -0700

    dh: rename again

--- nike/diffiehellman/dh.go
+++ nike/diffiehellman/dh.go
@@ -60,7 +60,7 @@ var _ nike.PublicKey = (*PublicKey)(nil)
 var _ nike.Scheme = (*dhNIKE)(nil)
 
 func (d *dhNIKE) Name() string {
-	return "ClassicDH_4096_RFC3526"
+	return "ClassicDH_4096_Group16"
 }
 
 func (d *dhNIKE) PublicKeySize() int {
