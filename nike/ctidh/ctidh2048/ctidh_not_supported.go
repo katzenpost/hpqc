580470a2a0fc56d57fe7afdeac4a80e533b9965c
David Stainton <dstainton415@gmail.com>
2024-05-17 14:53:27 -0700

    ctidh: don't build it on windows

---
+++ nike/ctidh/ctidh2048/ctidh_not_supported.go
@@ -0,0 +1,11 @@
+//go:build windows
+// +build windows
+
+// SPDX-FileCopyrightText: (c) 2024 David Stainton
+// SPDX-License-Identifier: AGPL-3.0-only
+
+package ctidh511
+
+import "github.com/katzenpost/hpqc/nike"
+
+func Scheme() nike.Scheme { return nil }
