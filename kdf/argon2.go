b07d0480ccb155daf8c93f2b8e7f02ca52b20914
David Stainton <dstainton415@gmail.com>
2024-08-16 18:30:41 -0700

    Add hkdf

--- kdf/argon2.go
+++ kdf/argon2.go
@@ -1,7 +1,11 @@
 package kdf
 
 import (
+	"hash"
+
 	"golang.org/x/crypto/argon2"
+	"golang.org/x/crypto/blake2b"
+	"golang.org/x/crypto/hkdf"
 )
 
 func Key(password, salt []byte, time, memory uint32, threads uint8, keyLen uint32) []byte {
@@ -10,4 +14,15 @@ func Key(password, salt []byte, time, memory uint32, threads uint8, keyLen uint3
 
 func IDKey(password, salt []byte, time, memory uint32, threads uint8, keyLen uint32) []byte {
 	return argon2.IDKey(password, salt, time, memory, threads, keyLen)
+}
+
+func HKDF(ikm, salt []byte) []byte {
+	h := func() hash.Hash {
+		h, err := blake2b.New256(nil)
+		if err != nil {
+			panic(err)
+		}
+		return h
+	}
+	return hkdf.Extract(h, ikm, salt)
 }
