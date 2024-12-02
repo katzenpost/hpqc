8e122c5a0ea25ef3c40f2123b4d559f612f9b6b4
cwd.systems | 0KN <101006007+infinitydaemon@users.noreply.github.com>
2024-12-02 17:11:25 +0600

    Update hash.go

--- hash/hash.go
+++ hash/hash.go
@@ -2,20 +2,23 @@ package hash
 
 import (
 	"encoding"
+	"fmt"
 
 	"golang.org/x/crypto/blake2b"
 )
 
 const HashSize = blake2b.Size256
 
-func Sum256(data []byte) [blake2b.Size256]byte {
+func Sum256(data []byte) [HashSize]byte {
 	return blake2b.Sum256(data)
 }
 
-func Sum256From(key encoding.BinaryMarshaler) [blake2b.Size256]byte {
+func Sum256From(key encoding.BinaryMarshaler) ([HashSize]byte, error) {
+	var zero [HashSize]byte // Return value in case of error
+
 	blob, err := key.MarshalBinary()
 	if err != nil {
-		panic(err)
+		return zero, fmt.Errorf("failed to marshal binary: %w", err)
 	}
-	return blake2b.Sum256(blob)
+	return blake2b.Sum256(blob), nil
 }
