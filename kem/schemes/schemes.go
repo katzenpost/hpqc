e724e20f20d5764dac2c57696712c212855acf38
cwd.systems | 0KN <101006007+infinitydaemon@users.noreply.github.com>
2024-12-02 17:10:01 +0600

    Update schemes.go
    Grouped schemes, added validation, added structured warnings, redundancy in init() by appending groups of schemes and validating them centrally and updated ByName to return errors for invalid lookups.

--- kem/schemes/schemes.go
+++ kem/schemes/schemes.go
@@ -1,6 +1,8 @@
 package schemes
 
 import (
+	"fmt"
+	"log"
 	"strings"
 
 	"github.com/katzenpost/circl/kem/frodo/frodo640shake"
@@ -15,7 +17,6 @@ import (
 	"github.com/katzenpost/circl/kem/mceliece/mceliece6960119f"
 	"github.com/katzenpost/circl/kem/mceliece/mceliece8192128"
 	"github.com/katzenpost/circl/kem/mceliece/mceliece8192128f"
-
 	"github.com/katzenpost/hpqc/kem"
 	"github.com/katzenpost/hpqc/kem/adapter"
 	"github.com/katzenpost/hpqc/kem/combiner"
@@ -32,46 +33,13 @@ import (
 	"github.com/katzenpost/hpqc/rand"
 )
 
-var potentialSchemes = [...]kem.Scheme{
-
-	// PQ KEMs
-
-	adapter.FromNIKE(ctidh511.Scheme()),
-	adapter.FromNIKE(ctidh512.Scheme()),
-	adapter.FromNIKE(ctidh1024.Scheme()),
-	adapter.FromNIKE(ctidh2048.Scheme()),
-
-	// hybrid KEMs
-
-	combiner.New(
-		"CTIDH512-X25519",
-		[]kem.Scheme{
-			adapter.FromNIKE(ctidh512.Scheme()),
-			adapter.FromNIKE(x25519.Scheme(rand.Reader)),
-		},
-	),
-	combiner.New(
-		"CTIDH1024-X448",
-		[]kem.Scheme{
-			adapter.FromNIKE(ctidh1024.Scheme()),
-			adapter.FromNIKE(x448.Scheme(rand.Reader)),
-		},
-	),
-}
-
-var allSchemes = []kem.Scheme{
-
-	// classical KEM schemes (converted from NIKE via hashed elgamal construction)
-
-	// Classical DiffieHellman imeplementation has a bug with this ticket:
-	// https://github.com/katzenpost/hpqc/issues/39
-	//adapter.FromNIKE(diffiehellman.Scheme()),
-
+// Group schemes into logical categories
+var classicalSchemes = []kem.Scheme{
 	adapter.FromNIKE(x25519.Scheme(rand.Reader)),
 	adapter.FromNIKE(x448.Scheme(rand.Reader)),
+}
 
-	// post quantum KEM schemes
-
+var postQuantumSchemes = []kem.Scheme{
 	mlkem768.Scheme(),
 	sntrup.Scheme(),
 	frodo640shake.Scheme(),
@@ -85,21 +53,9 @@ var allSchemes = []kem.Scheme{
 	mceliece6960119f.Scheme(),
 	mceliece8192128.Scheme(),
 	mceliece8192128f.Scheme(),
+}
 
-	// hybrid KEM schemes
-
-	xwing.Scheme(),
-
-	// XXX TODO: must soon deprecate use of "hybrid.New" in favour of "combiner.New".
-	// We'd also like to remove Kyber now that we have mlkem768.
-	hybrid.New(
-		"Kyber768-X25519",
-		adapter.FromNIKE(x25519.Scheme(rand.Reader)),
-		kyber768.Scheme(),
-	),
-
-	// If Xwing is not the PQ Hybrid KEM you are looking for then we recommend
-	// using our secure generic KEM combiner:
+var hybridSchemes = []kem.Scheme{
 	combiner.New(
 		"MLKEM768-X25519",
 		[]kem.Scheme{
@@ -114,102 +70,46 @@ var allSchemes = []kem.Scheme{
 			mlkem768.Scheme(),
 		},
 	),
-
-	// all the Classic McEliece's from our fork of circl
-	combiner.New(
-		"mceliece348864-X25519",
-		[]kem.Scheme{
-			adapter.FromNIKE(x25519.Scheme(rand.Reader)),
-			mceliece348864.Scheme(),
-		},
-	),
-	combiner.New(
-		"mceliece348864f-X25519",
-		[]kem.Scheme{
-			adapter.FromNIKE(x25519.Scheme(rand.Reader)),
-			mceliece348864f.Scheme(),
-		},
-	),
-	combiner.New(
-		"mceliece460896-X25519",
-		[]kem.Scheme{
-			adapter.FromNIKE(x25519.Scheme(rand.Reader)),
-			mceliece460896.Scheme(),
-		},
-	),
-	combiner.New(
-		"mceliece460896f-X25519",
-		[]kem.Scheme{
-			adapter.FromNIKE(x25519.Scheme(rand.Reader)),
-			mceliece460896f.Scheme(),
-		},
-	),
-	combiner.New(
-		"mceliece6688128-X25519",
-		[]kem.Scheme{
-			adapter.FromNIKE(x25519.Scheme(rand.Reader)),
-			mceliece6688128.Scheme(),
-		},
-	),
-	combiner.New(
-		"mceliece6688128f-X25519",
-		[]kem.Scheme{
-			adapter.FromNIKE(x25519.Scheme(rand.Reader)),
-			mceliece6688128f.Scheme(),
-		},
-	),
-	combiner.New(
-		"mceliece6960119-X25519",
-		[]kem.Scheme{
-			adapter.FromNIKE(x25519.Scheme(rand.Reader)),
-			mceliece6960119.Scheme(),
-		},
-	),
-	combiner.New(
-		"mceliece6960119f-X25519",
-		[]kem.Scheme{
-			adapter.FromNIKE(x25519.Scheme(rand.Reader)),
-			mceliece6960119f.Scheme(),
-		},
-	),
-	combiner.New(
-		"mceliece8192128-X25519",
-		[]kem.Scheme{
-			adapter.FromNIKE(x25519.Scheme(rand.Reader)),
-			mceliece8192128.Scheme(),
-		},
-	),
-	combiner.New(
-		"mceliece8192128f-X25519",
-		[]kem.Scheme{
-			adapter.FromNIKE(x25519.Scheme(rand.Reader)),
-			mceliece8192128f.Scheme(),
-		},
-	),
 }
 
+// Collect all schemes together
+var allSchemes []kem.Scheme
 var allSchemeNames map[string]kem.Scheme
 
+// Initialize all schemes and validate them
 func init() {
 	allSchemeNames = make(map[string]kem.Scheme)
-	for _, scheme := range potentialSchemes {
-		if scheme != nil {
-			allSchemes = append(allSchemes, scheme)
+	allSchemes = append(allSchemes, classicalSchemes...)
+	allSchemes = append(allSchemes, postQuantumSchemes...)
+	allSchemes = append(allSchemes, hybridSchemes...)
+
+	for _, scheme := range allSchemes {
+		if err := validateScheme(scheme); err == nil {
+			allSchemeNames[strings.ToLower(scheme.Name())] = scheme
+		} else {
+			log.Printf("Warning: %v", err)
 		}
 	}
-	for _, scheme := range allSchemes {
-		allSchemeNames[strings.ToLower(scheme.Name())] = scheme
+}
+
+// Validate a scheme to ensure it is not nil and has a valid name
+func validateScheme(scheme kem.Scheme) error {
+	if scheme == nil || strings.TrimSpace(scheme.Name()) == "" {
+		return fmt.Errorf("invalid scheme: %v", scheme)
 	}
+	return nil
 }
 
-// ByName returns the NIKE scheme by string name.
-func ByName(name string) kem.Scheme {
-	ret := allSchemeNames[strings.ToLower(name)]
-	return ret
+// ByName retrieves a scheme by name, returning an error if not found
+func ByName(name string) (kem.Scheme, error) {
+	scheme, ok := allSchemeNames[strings.ToLower(name)]
+	if !ok {
+		return nil, fmt.Errorf("scheme '%s' not found", name)
+	}
+	return scheme, nil
 }
 
-// All returns all NIKE schemes supported.
+// All returns a slice of all supported schemes
 func All() []kem.Scheme {
-	a := allSchemes
-	return a[:]
+	return allSchemes
 }
